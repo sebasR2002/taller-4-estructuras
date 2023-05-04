1370d273e0a1f188bff445ae3800d937cd44d392
SEBASTIAN RESTREPO <sebas20022010@gmail.com>
2023-05-03 20:16:13 -0500

    se agregan clases delayrecord y analisis

---
+++ src/AnalisisDatos.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.*;
+
+public class AnalisisDatos {
+    public static void main(String[] args) throws Exception {
+        String route = "548634059_T_ONTIME_REPORTING.csv";
+        System.out.println("Inicio de lectura");
+        // ArrayList<DelayRecord> a = DelayRecord.readCSVFile(route);
+        System.out.println("Fin lectura");
+    }
+
+    public ST<String, Integer> meanRouteDelay(ArrayList<DelayRecord> delays) {
+        ST<String, Integer> T = new ST<>();
+
+        return T;
+    }
+}
